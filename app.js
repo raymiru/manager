de6e04c04ad6dd4ec56932646c7a85a4b0461238
Ray Miru <raymiru.one@gmail.com>
2019-06-20 08:37:01 +0700

    20.06

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const config = require('config');
 const express = require('express');
-const write = require('./utilities/consoleWrap')(config.get('logLevel'));// Если 1, то пушутся только error и info, если 2 - то все, если 0 - то ничего.
+const write = require('./src/utilities/consoleWrap')(config.get('logLevel'));// Если 1, то пушутся только error и info, если 2 - то все, если 0 - то ничего.
 const app = express();
+const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
-const websocket = require('./websocket')(io);
-const db = require('./services/db/index')();
+const websocket = require('./src/websocket')(io);
+const db = require('./src/services/db')();
 
 
 const port = process.env.PORT || '3500';
+app.use(express.static(path.join(__dirname, './dist')));
 
 
 app.set('port', port);
